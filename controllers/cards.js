acbaae485dad73ff87edca2f73fe6384c318be99
Timofey Berezhnov <timofeus91@gmail.com>
2021-05-11 14:42:48 +0400

    new routes ready

--- controllers/cards.js
+++ controllers/cards.js
@@ -19,3 +19,23 @@ module.exports.deleteCard = (req, res) => {
     .then((card) => res.send(card))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },
+  )
+    .then((card) => res.send(card))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true },
+  )
+    .then((card) => res.send(card))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 
-const { getUsers, getUser, createUser } = require('../controllers/users');
+const {
+  getUsers, getUser, createUser, updateInfo, updateAvatar,
+} = require('../controllers/users');
 
 router.get('/users', getUsers);
-router.get('/users/:id', getUser);
+router.get('/users/:userId', getUser);
 router.post('/users', createUser);
+router.patch('/users/me', updateInfo);
+router.patch('/users/me/avatar', updateAvatar);
 
 module.exports = router;
